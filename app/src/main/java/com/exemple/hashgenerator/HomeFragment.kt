9ad119406611d5e450a170ff6639ad5ec0d68c7c
Jvitaliy <jvit82@gmail.com>
2021-06-19 21:43:31 +0700

    add animations

--- app/src/main/java/com/exemple/hashgenerator/HomeFragment.kt
+++ app/src/main/java/com/exemple/hashgenerator/HomeFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.exemple.hashgenerator.databinding.FragmentHomeBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -22,7 +26,46 @@ class HomeFragment : Fragment() {
         val hashAlgorithms = resources.getStringArray(R.array.hash_algorilms)
         val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hashAlgorithms)
         binding.autoCompleteTextView.setAdapter(arrayAdapter)
+
+        binding.generateButton.setOnClickListener {
+            lifecycleScope.launch {
+                applyAnimations()
+                navigateToSuccess()
+            }
+        }
+
         return binding.root
+    }
+
+    private suspend fun applyAnimations() {
+        binding.titleTextView.animate().alpha(0f).duration = 400L
+        binding.generateButton.animate().alpha(0f).duration = 400L
+        binding.textInputLayout.animate()
+            .alpha(0f)
+            .translationXBy(1200f)
+            .duration = 400L
+        binding.plainText.animate()
+            .alpha(0f)
+            .translationXBy(-1200f)
+            .duration = 400L
+
+        delay(300)
+
+        binding.successBackground.animate().alpha(1f).duration = 600L
+        binding.successBackground.animate().rotationBy(720f).duration = 600L
+        binding.successBackground.animate().scaleXBy(900f).duration = 800L
+        binding.successBackground.animate().scaleYBy(900f).duration = 800L
+
+        delay(500)
+
+        binding.successImageView.animate().alpha(1f).duration = 1000L
+
+        delay(1500)
+
+    }
+
+    private fun navigateToSuccess() {
+        findNavController().navigate(R.id.action_homeFragment_to_successFragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
