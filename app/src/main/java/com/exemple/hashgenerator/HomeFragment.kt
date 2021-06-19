ea7376f64ad111528bee73197d6fa7854e9a2c4f
Jvitaliy <jvit82@gmail.com>
2021-06-19 20:53:18 +0700

    home screen

---
+++ app/src/main/java/com/exemple/hashgenerator/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.exemple.hashgenerator
+
+import android.os.Bundle
+import android.view.*
+import android.widget.ArrayAdapter
+import androidx.fragment.app.Fragment
+import com.exemple.hashgenerator.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        setHasOptionsMenu(true)
+
+        val hashAlgorithms = resources.getStringArray(R.array.hash_algorilms)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hashAlgorithms)
+        binding.autoCompleteTextView.setAdapter(arrayAdapter)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
